466ed86bc68b2fefdc5b7357c18694b45708814d
MolanoAlternova <santiago.molano@alternova.com>
2023-07-24 13:13:20 -0500

    asdf

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -6,7 +6,7 @@ export const aboutMeConfig = {
     topLine: 'About Me',
     headline: 'Fullstack Developer',
     description: "Hey there! I'm Santiago Molano, a 20-year-old software enthusiast from Colombia. With a strong background in software development, I've dived into projects that have honed my skills. Passionate about the ever-evolving nature of technology, I'm always eager to learn something new. Whether it's mastering a new programming language, exploring cutting-edge frameworks, or staying up-to-date with the latest industry trends, I thrive on continuous growth and self-improvement.",
-    buttonLabel: 'See cv',
+    buttonLabel: 'See CV',
     imgStart: true,
     img: require('../images/AboutMe.png'),
     alt: 'My photo',
