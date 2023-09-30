5269388586c4da99ff28df91cae2c422ed2b9129
MolanoAlternova <santiago.molano@alternova.com>
2023-09-30 11:03:57 -0500

    Update about me photo

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -8,7 +8,7 @@ export const aboutMeConfig = {
     description: "Hey there! I'm Santiago Molano, a 20-year-old software enthusiast from Colombia. With a strong background in software development, I've dived into projects that have honed my skills. Passionate about the ever-evolving nature of technology, I'm always eager to learn something new. Whether it's mastering a new programming language, exploring cutting-edge frameworks, or staying up-to-date with the latest industry trends, I thrive on continuous growth and self-improvement.",
     buttonLabel: 'Download resume',
     imgStart: true,
-    img: require('../images/AboutMe2.png'),
+    img: require('../images/photo.jpeg'),
     alt: 'My photo',
     dark: false,
     primary: true,
