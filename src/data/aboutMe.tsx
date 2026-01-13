a1884f8b5484251d18aa7f7ed9ed6931f7be645b
Santiago Molano <102083840+molxno@users.noreply.github.com>
2026-01-13 10:59:25 -0500

    Update aboutMe.tsx

--- src/data/aboutMe.tsx
+++ src/data/aboutMe.tsx
@@ -6,7 +6,7 @@ export const aboutMeConfig = {
   topLine: 'About Me',
   headline: 'Tech Lead & Software Architect | PHP (Laravel), Clean Architecture, Fintech',
   description: `
-        I’m Santiago Molano, a backend developer with 3+ years of experience leading Laravel projects in fintech and banking. I specialize in clean architecture, scalable APIs, and secure deployments. I’ve led technical initiatives, mentored developers, and taken ownership of critical backend systems.
+        I’m Santiago Molano, a backend developer with 4+ years of experience leading Laravel projects in fintech and banking. I specialize in clean architecture, scalable APIs, and secure deployments. I’ve led technical initiatives, mentored developers, and taken ownership of critical backend systems.
         Certified in cybersecurity and currently enhancing my English (B1 → B2), I’m focused on stepping into a Tech Lead or Software Architect role — where strategy meets code and impact is measurable.
     `,
   buttonLabel: 'Download Curriculum Vitae',
